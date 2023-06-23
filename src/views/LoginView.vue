bc432137aea73365c4b8f46cdb5fe2e20a9579b0
Abduqodir0205 <abduqodirDeveloper@gmail.com>
2023-06-23 08:55:34 -0700

    start__IT__center

---
+++ src/views/LoginView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login">
+        <div class="container">
+            <div class="login__content">
+                <div class="login__left-item">
+                    <h2>Welcome Back!</h2>
+                    <form class="login__form" action="#">
+                        <span>Username:</span>
+                        <input type="text">
+                        <span>Password:</span>
+                        <input type="password">
+                        <button>Login</button>
+                    </form>
+                    <p>Dont  have and account?<span>Register</span></p>
+                    <div class="icons"></div>
+                </div>
+                <div class="login__right-item">
+                    <img src="../assets/images/png/Other 03.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="login__rot"></div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
