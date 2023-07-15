acd9677e8525d58681dc2795f7a78983662b3ef0
Abdulhamidfrontedd <abdulhamidturgunov1606@gmail.com>
2023-07-15 10:38:21 +0500

    table_Senior_Devlopeer

---
+++ src/views/TableView.vue
@@ -0,0 +1,368 @@
+<template lang="">
+    <div class="table">
+        <div class="table__top">
+            <p class="table__top__p1">Home</p>
+            <img class="table__top__img" src="../assets/images/svg/Vector2.svg" alt="">
+            <p class="table__top__p">E-commerce</p>
+            <img class="table__top__img" src="../assets/images/svg/Vector2.svg" alt="">
+            <p class="table__top__p">Orders</p>
+            <img class="table__top__img" src="../assets/images/svg/Vector2.svg" alt="">
+        </div>
+        <div class="table__middle">
+            <div class="table__middle__left">
+                <h1 class="table__middle__h1">Hisobotlar</h1>
+            <div class="table__middle__child">
+                <button class="table__middle__child__title">All 87</button>
+                <button class="table__middle__child__title add__class">Paid 15</button>
+                <button class="table__middle__child__title add__class">Due 67</button>
+                <button class="table__middle__child__title add__class">Overdue 12</button>
+            </div>
+            </div>
+            <div class="table__middle__right">
+            <div class="table__middle__section">
+                <div class="table__middle__inp">
+                    <img class="table__middle__img2" src="../assets/images/svg/Search.svg" alt="">
+                    <input class="table__middle__from" type="text" placeholder="Search by invonice ID...">
+            </div>
+            <div class="table__middle__btn">
+                <img src="../assets/images/svg/Add_Plus.svg" alt="">
+                <button class="table__middle__button">Add Order</button>
+            </div>
+            </div>
+            <div class="table__middle__hugo">
+                <div class="table__middle__thedate">
+                <img src="../assets/images/svg/calendar.svg" alt="">
+                <p class="table__middle__hugo__p">Last 7 Days</p>
+                <img src="../assets/images/svg/sitebar-bottom.svg" alt="">
+            </div>
+            <div class="table__middle__list">
+                <img src="../assets/images/svg/Buttons.svg" alt="">
+            </div>
+            </div>
+            </div>
+        </div>
+        <div class="table__bottom">
+            <h4 class="table__bottom__h4">Invoices 87</h4>
+            <div class="table__bottom__child">
+                <div class="table__bottom__total">
+                   <input type="checkbox"> 
+                    <p class="invois">Invoices</p>
+                    <div class="total">
+                        <p class="total__p">Total</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <div class="total">
+                        <p class="total__p">Status</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <div class="total">
+                        <p class="total__p">CUSTOMERS</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <div class="total">
+                        <p class="total__p">ISSUED ON</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <div class="total">
+                        <p class="total__p">PAID ON</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <div class="total">
+                        <p class="total__p">TYPE</p>
+                        <img src="../assets/images/svg/dubl.svg" alt="">
+                    </div>
+                    <p class="action">ACtION</p>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$85694.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title1">Paid</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Lidia Semonenko</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">20/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$129.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title1">Paid</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">ANNAfONENKO</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">19/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$45.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title1">Paid</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Anna Dymenko</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">19/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/repeat.svg" alt="">
+                        <p class="total__sub">One Time</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                <div class="table__bottom__title">
+                    <input type="checkbox"> 
+                    <p class="title__invois">#164675</p>
+                    <div class="total__title">
+                        <p class="title__p">$4567.00</p>
+                    </div>
+                    <div class="total__title">
+                        <p class=" color__title">Overdue</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="my__subtitle">Mykola Semen</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__data">22/04/2022</p>
+                    </div>
+                    <div class="total__title">
+                        <p class="title__minus">--</p>
+                    </div>
+                    <div class="total__title">
+                        <img  class="refresh" src="../assets/images/svg/rotate-left.svg" alt="">
+                        <p class="total__sub">Subscription</p>
+                    </div>
+                    <div class="title__action">
+                        <img src="../assets/images/svg/Edit_Pencil_01.svg" alt="">
+                        <img src="../assets/images/svg/Dowmload.svg" alt="">
+                        <img src="../assets/images/svg/Trash can.svg" alt="">
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
